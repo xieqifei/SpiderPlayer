6c8c8b48644ef9aff354215bbb6cf21751132b14
xieqifei <me@xieqifei.com>
2021-02-27 11:47:34 +0100

    提前激活云函数

--- front/player.py
+++ front/player.py
@@ -7,8 +7,12 @@
 @version      :1.0
 '''
 
+import requests
 
 def main_handler(event,context):
+    baseUrl = "https://service-ife10uoc-1258461674.hk.apigw.tencentcs.com/release/music-api"
+    #当返回html页面时，get一次后端api，激活云函数，这样前端html从api获取json时，api云函数会更快做出响应
+    requests.get(baseUrl)
     with open('./index.html') as f:
         html = f.read()
     return {
